bc87539bcfac12c0c8af3cd72edb8d5b8c0547af
Manvendra Singh <msin0017@student.monash.edu>
2023-05-01 20:34:46 +1000

    fizzbuzz

--- challenges/intro-to-c/fizzbuzz/fizzbuzz.c
+++ challenges/intro-to-c/fizzbuzz/fizzbuzz.c
@@ -1,1 +1,26 @@
 /// Implementation
+#include<stdio.h>
+
+int main(){
+int i;
+    
+    for (i = 0; i <= 100; i++)
+    {
+        if(i%3 == 0 && i%5 == 0){
+            printf("Fizzbuzz\n");
+            continue;
+        }
+        else if(i%3 == 0){
+            printf("Fizz\n");
+            continue;
+        }
+        else if(i%5 == 0){
+            printf("Buzz\n");
+            continue;
+        }
+        else{
+            printf("%d\n", i);
+            continue;
+        }
+    }
+}
