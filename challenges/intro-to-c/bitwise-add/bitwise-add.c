5ab3cd06ea8babc0d609f940ea1e6073480182e4
Manvendra Singh <msin0017@student.monash.edu>
2023-05-08 15:58:02 +1000

    bitwise multiply

--- challenges/intro-to-c/bitwise-add/bitwise-add.c
+++ challenges/intro-to-c/bitwise-add/bitwise-add.c
@@ -1,4 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 int bitwise_add(int x, int y) {
     while (y != 0){
         int carry = x & y;
@@ -7,8 +9,32 @@ int bitwise_add(int x, int y) {
         }
     return x;
 }
+int bitwise_mul(int x, int y) {
+    int res = 0;
+    bool positive = false;
+    if (x >= 0 && y >=0)
+    { 
+        positive = true;
+    }
+    else if (x < 0 && y < 0){
+        positive = true;
+    }
+    x = abs(x);
+    y = abs(y);
+    int i;
+    for (i = 0; i < y; i++){
+        res = bitwise_add(res, x);
+    }
+    if(positive){
+        return res;
+    }
+    else{
+        return -res;
+    }
+}
 int main(){
-   int a = 100, b = 100;
+   int a = 56, b = -3;
    printf("The sum of %d and %d using bitwise adding is %d", a, b, bitwise_add(a, b));
+   printf("\nThe product of %d and %d using bitwise adding is %d", a, b, bitwise_mul(a, b));
    return 0;
 }
