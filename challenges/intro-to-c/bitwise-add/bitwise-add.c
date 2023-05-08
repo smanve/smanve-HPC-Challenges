bf97c414cd53e77ed28f629ef168f25556bb2187
Manvendra Singh <msin0017@student.monash.edu>
2023-05-08 15:22:56 +1000

    bitwise-add

--- challenges/intro-to-c/bitwise-add/bitwise-add.c
+++ challenges/intro-to-c/bitwise-add/bitwise-add.c
@@ -1,1 +1,14 @@
-/// Implementation
+#include <stdio.h>
+int bitwise_add(int x, int y) {
+    while (y != 0){
+        int carry = x & y;
+        x = x ^ y; 
+        y = carry << 1;
+        }
+    return x;
+}
+int main(){
+   int a = 100, b = 100;
+   printf("The sum of %d and %d using bitwise adding is %d", a, b, bitwise_add(a, b));
+   return 0;
+}
